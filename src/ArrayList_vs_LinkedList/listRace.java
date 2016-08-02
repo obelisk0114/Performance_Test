bfd17c88acba6589afc8423c323f73f1031645a5
obelisk0114 <obelisk0114@yahoo.com.tw>
2016-08-03 04:15:57 +0800

    Test remove element
    Test remove element

--- src/ArrayList_vs_LinkedList/listRace.java
+++ src/ArrayList_vs_LinkedList/listRace.java
@@ -2,16 +2,17 @@ package ArrayList_vs_LinkedList;
 
 /*
  * http://oranwind.org/eclipse-chang-yong-kuai-su-jian-ji-zi-dong-wan-cheng-auto-complete/
+ * http://www.cnblogs.com/skywang12345/p/3308807.html
  */
 
-import java.util.List;
+//import java.util.List;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Iterator;
 
 public class listRace {
-	private List<String> ArrayList_String;
-	private List<String> LinkedList_String;
+	private ArrayList<String> ArrayList_String;
+	private LinkedList<String> LinkedList_String;
 	public listRace(int n, int m) {
 		fillArrayList(n);
 		fillLinkedList(m);
@@ -32,19 +33,26 @@ public class listRace {
 		ArrayList_every_size(n);
 		ArrayList_once_size(n);
 		ArrayList_decrease(n);
+		ArrayList_removeFirst(n);
+		ArrayList_removeLast(n);
+//		ArrayList_removeAll(n);
 	}
 	
 	public void listRace_LinkedList(int n) {
 		LinkedList_for_each(n);
 		LinkedList_iterator(n);
-		LinkedList_every_size(n);
-		LinkedList_once_size(n);
-		LinkedList_decrease(n);		
+//		LinkedList_every_size(n);
+//		LinkedList_once_size(n);
+//		LinkedList_decrease(n);
+		LinkedList_pollFirst(n);
+		LinkedList_pollLast(n);
+		LinkedList_removeFirst(n);
+		LinkedList_removeLast(n);
 	}
 	
 	public void fillArrayList(int n) {
 		long preTime = System.currentTimeMillis();
-		List<String> data1 = new ArrayList<String>(n);
+		ArrayList<String> data1 = new ArrayList<String>(n);
 		for (int i = 0; i < n; i++) {
 			data1.add(i + "");
 		}
@@ -57,7 +65,7 @@ public class listRace {
 	
 	public void fillLinkedList(int n) {
 		long preTime = System.currentTimeMillis();
-		List<String> data2 = new LinkedList<String>();
+		LinkedList<String> data2 = new LinkedList<String>();
 		for (int i = 0; i < n; i++) {
 			data2.add(i + "");
 		}
@@ -120,6 +128,51 @@ public class listRace {
 		System.out.println("ArrayList decrease Run time : " + currentTime + " millisec");
 	}
 	
+	public void ArrayList_removeFirst(int n) {
+		ArrayList<String> datatest = new ArrayList<String>();
+		for (int i = 0; i < n; i++) {
+			datatest.add(i + "");
+		}
+		
+		int size = datatest.size();
+		long preTime = System.currentTimeMillis();
+		for (int i = size - 1; i >= 0; i--) {
+			datatest.remove(0);
+		}
+		long aftTime = System.currentTimeMillis();
+		long currentTime = aftTime - preTime;
+		System.out.println("ArrayList removeFirst Run time : " + currentTime + " millisec");
+	}
+	
+	public void ArrayList_removeLast(int n) {
+		ArrayList<String> datatest = new ArrayList<String>();
+		for (int i = 0; i < n; i++) {
+			datatest.add(i + "");
+		}
+		
+		int size = datatest.size();
+		long preTime = System.currentTimeMillis();
+		for (int i = size - 1; i >= 0; i--) {
+			datatest.remove(i);
+		}
+		long aftTime = System.currentTimeMillis();
+		long currentTime = aftTime - preTime;
+		System.out.println("ArrayList removeLast Run time : " + currentTime + " millisec");
+	}
+	
+	public void ArrayList_removeAll(int n) {
+		ArrayList<String> datatest = new ArrayList<String>();
+		for (int i = 0; i < n; i++) {
+			datatest.add(i + "");
+		}
+		
+		long preTime = System.currentTimeMillis();
+		datatest.removeAll(datatest);
+		long aftTime = System.currentTimeMillis();
+		long currentTime = aftTime - preTime;
+		System.out.println("ArrayList removeAll Run time : " + currentTime + " millisec");
+	}
+	
 	public void LinkedList_for_each(int n) {
 		long preTime = System.currentTimeMillis();
 		for (String element : LinkedList_String) {
@@ -170,6 +223,66 @@ public class listRace {
 		long aftTime = System.currentTimeMillis();
 		long currentTime = aftTime - preTime;
 		System.out.println("LinkedList decrease Run time : " + currentTime + " millisec");
+	}
+	
+	public void LinkedList_pollFirst(int n) {
+		LinkedList<String> datatest = new LinkedList<String>();
+		for (int i = 0; i < n; i++) {
+			datatest.add(i + "");
+		}
+		
+		long preTime = System.currentTimeMillis();
+		while(!datatest.isEmpty()) {
+			datatest.pollFirst();
+		}
+		long aftTime = System.currentTimeMillis();
+		long currentTime = aftTime - preTime;
+		System.out.println("LinkedList poll first Run time : " + currentTime + " millisec");
+	}
+	
+	public void LinkedList_pollLast(int n) {
+		LinkedList<String> datatest = new LinkedList<String>();
+		for (int i = 0; i < n; i++) {
+			datatest.add(i + "");
+		}
+		
+		long preTime = System.currentTimeMillis();
+		while(!datatest.isEmpty()) {
+			datatest.pollLast();
+		}
+		long aftTime = System.currentTimeMillis();
+		long currentTime = aftTime - preTime;
+		System.out.println("LinkedList poll Last Run time : " + currentTime + " millisec");
+	}
+	
+	public void LinkedList_removeFirst(int n) {
+		LinkedList<String> datatest = new LinkedList<String>();
+		for (int i = 0; i < n; i++) {
+			datatest.add(i + "");
+		}
+		
+		long preTime = System.currentTimeMillis();
+		while (!datatest.isEmpty()) {
+			datatest.removeFirst();
+		}
+		long aftTime = System.currentTimeMillis();
+		long currentTime = aftTime - preTime;
+		System.out.println("LinkedList remove First Run time : " + currentTime + " millisec");
+	}
+	
+	public void LinkedList_removeLast(int n) {
+		LinkedList<String> datatest = new LinkedList<String>();
+		for (int i = 0; i < n; i++) {
+			datatest.add(i + "");
+		}
+		
+		long preTime = System.currentTimeMillis();
+		while (!datatest.isEmpty()) {
+			datatest.removeLast();
+		}
+		long aftTime = System.currentTimeMillis();
+		long currentTime = aftTime - preTime;
+		System.out.println("LinkedList remove Last Run time : " + currentTime + " millisec");
 	}
 
 }
--- src/ArrayList_vs_LinkedList/testArea.java
+++ src/ArrayList_vs_LinkedList/testArea.java
@@ -4,8 +4,8 @@ public class testArea {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n = 10000;
-		int m = 10000;
+		int n = 100000;
+		int m = 100000;
 		new listRace(n, m);
 
 	}
